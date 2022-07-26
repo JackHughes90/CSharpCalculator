ffb73c4abcb3ed8fdb051c28bc81990de6500f10
jhughes <jackawhughes@gmail.com>
2022-07-26 14:41:43 +0200

    first commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calculator // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the calculator!");
+            Console.WriteLine("Enter a number:");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter another number:");
+            int number2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("The sum of these two numbers is " + (number1 + number2));
+            Console.ReadLine();
+        }
+    }
+}
