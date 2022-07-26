7363a2e78fe8443a45add12ce50c2e85fdbf21de
jhughes <jackawhughes@gmail.com>
2022-07-26 15:15:37 +0200

    Added functionality, including choice of mathematical operator!

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,11 +7,35 @@ namespace Calculator // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the calculator!");
-            Console.WriteLine("Enter a number:");
+            
+            Console.WriteLine("Please enter an operator: (*/+-)");
+            string inputOp = Console.ReadLine();
+            
+            Console.WriteLine("Enter your first number:");
             int number1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter another number:");
+            
+            Console.WriteLine("Enter your second number:");
             int number2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("The sum of these two numbers is " + (number1 + number2));
+
+            switch (inputOp)
+            {
+                case "*":
+                    Console.WriteLine("The product of these two numbers is " + (number1 * number2));
+                    break;
+                case "/":
+                    Console.WriteLine("First number divided by Second Number is " + (number1 / number2));
+                    break;
+                case "+":
+                    Console.WriteLine("The sum of these two numbers is " + (number1 + number2));
+                    break;
+                case "-":
+                    Console.WriteLine("First Number minus Second Number is " + (number1 - number2));
+                    break;
+                default:
+                    Console.WriteLine("Sorry, I think you fucked up...");
+                    break;
+            }
+            
             Console.ReadLine();
         }
     }
